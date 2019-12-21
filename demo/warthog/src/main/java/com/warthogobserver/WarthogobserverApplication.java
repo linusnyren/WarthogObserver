524422c8fd9c1695333db8a24ad28715da80e357
linusnyren <linusny@hotmail.com>
2019-12-21 13:08:59 +0100

    Initialized springboot and react project

---
+++ demo/warthog/src/main/java/com/warthogobserver/WarthogobserverApplication.java
@@ -0,0 +1,13 @@
+package com.warthogobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarthogobserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarthogobserverApplication.class, args);
+	}
+
+}
---
+++ demo/warthog/src/test/java/com/warthogobserver/WarthogobserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warthogobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarthogobserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
